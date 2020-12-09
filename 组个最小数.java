fe970208771ca92ac7c0f2593bb05f28f1ff3428
Java <15591459159@163.com>
2020-12-09 23:06:01 +0800

    编程题：组个最小数

---
+++ 组个最小数.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class test2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int i = 1; i < 10; i++) {
+            if(arr[i] != 0) {
+                System.out.print(i);
+                arr[i]--;
+                break;
+            }
+        }
+        while (arr[0] != 0) {
+            System.out.print(0);
+            arr[0]--;
+        }
+        for (int i = 1; i < 10; i++) {
+            while(arr[i] != 0) {
+                System.out.print(i);
+                arr[i]--;
+            }
+        }
+    }
+}
