16d4b19d0ed5e267c41526c7fc0828a5445ce4e1
Java <15591459159@163.com>
2020-12-20 21:57:16 +0800

    连续子数组的最大和

---
+++ Day17.java
@@ -0,0 +1,20 @@
+public class FindGreatestSumOfSubArray {
+    public  int maxSum(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        int total = array[0];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (total >= 0) {
+                total += array[i];
+            } else {
+                total = array[i];
+            }
+            if (total > max) {
+                max = total;
+            }
+        }
+        return max;
+    }
+}
