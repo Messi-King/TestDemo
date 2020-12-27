106438dfb283c1332f133fe90c794a8d8ca02ecd
Java <15591459159@163.com>
2020-12-27 20:20:22 +0800

    mvc

---
+++ SpringMVC注解.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test2Controller {
+
+
+    @RequestMapping("/test2")
+    public String test2() {
+        return "forward:/home.html";
+    }
+}
