ba04ee9c0771b7aecd18d8f330cb4bdd73eff64f
Java <15591459159@163.com>
2021-01-13 22:46:02 +0800

    末尾0的个数

---
+++ 末尾0的个数.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]) {
+        Scanner sca = new Scanner(System.in);
+        while(sca.hasNext()) {
+            int a = sca.nextInt();
+            int count = 0;
+            for(int i=a;i>0;i--) {
+                int tmp = i;
+                while(tmp%5==0) {
+                    count++;
+                    tmp /= 5;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
